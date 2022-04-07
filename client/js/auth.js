191448e7015f80bfcb4c5aeff6ce8e2fb4bf27c8
Rees Varney <reesvarney02@gmail.com>
2022-04-07 17:10:58 +0100

    Disable identity button dynamically

--- client/js/auth.js
+++ client/js/auth.js
@@ -1,12 +1,19 @@
-import render from "./templates.js";
-import request from "./request.js";
-export default ()=>{
-  document.getElementById("page_content").innerHTML = render("identity", {createOrJoin: (window.gameData.players.length === 0) ? "Create" : "Join"});
-  document.getElementById("identity_form").addEventListener("submit", async(e)=>{
+import render from './templates.js';
+import request from './request.js';
+export default () => {
+  document.getElementById('page_content').innerHTML = render('identity', { createOrJoin: (window.gameData.players.length === 0) ? 'Create' : 'Join' });
+  document.querySelector('#identity_form input').addEventListener('input', (e) => {
+    if (e.target.value.length > 0) {
+      document.querySelector('#identity_form button').disabled = false;
+    } else {
+      document.querySelector('#identity_form button').disabled = true;
+    }
+  });
+  document.getElementById('identity_form').addEventListener('submit', async (e) => {
     e.preventDefault();
     const data = new FormData(e.target);
-    const success = await request.POST(`/api/${window.gameId}/join`, {
-      name: data.get("name")
+    await request.POST(`/api/${window.gameId}/join`, {
+      name: data.get('name'),
     });
-  })
-}
+  });
+};
