0555b725908b609ac6073767ca77c09f667c38a1
Rees Varney <reesvarney02@gmail.com>
2022-04-16 17:19:41 +0100

    Improved words

--- game/wordlists/getWords.js
+++ game/wordlists/getWords.js
@@ -1,22 +1,32 @@
 import fs from 'fs/promises';
 
-const file = await fs.readFile('./words.csv');
+const file = await fs.readFile('./google.csv');
 const data = file.toString().split('\n').map(e => e.trim()).map(e => e.split(',').map(e => e.trim()));
+console.log(data);
 const bad_file = await fs.readFile('./bad-words.txt');
 const bad_data = bad_file.toString().split('\n');
 const split = {};
-for(const wordLength of [...Array(8).keys()].map(a => a + 4)){
-  split[wordLength] = data.filter(([a,b,c])=> c.length === wordLength 
-  && /^[a-zA-Z]+$/.test(c)
-  && !bad_data.includes(c)
-  ).map(([a,b,c])=> c);
+for(const wordLength of [...Array(17).keys()].map(a => a + 4)){
+  split[wordLength] = [];
+}
+
+const exp = new RegExp(bad_data.join("|"));
+for(const [pos, word] of data){
+  if(
+    word.length > 3 && word.length < 21 &&
+    /^[a-zA-Z]+$/.test(word)
+    && !(exp.test(word))
+  ){
+    if(split[word.length].length < 1000){
+      split[word.length].push(word.toLowerCase());
+    }
+  }
 }
 
 console.log(Object.values(split).map(a=> a.length))
 
 
 for(const [length, words] of Object.entries(split)){
-  console.log(length, words.length)
   const top = words.splice(0, 300);
   fs.writeFile(`./${length}_chars.json`, JSON.stringify(top, null, 2));
 }
